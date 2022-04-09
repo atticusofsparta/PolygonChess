5bc3fb186908947295a6e2a11a9fb6f39f7fe833
Bobinstein <62410139+Bobinstein@users.noreply.github.com>
2022-04-09 19:08:36 -0400

    Creative block, so I'm uploading

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,52 @@
 import React from "react";
-import "./Style.css"
+import "./Style.css";
 
 const Home = () => {
+  const wallet =
+    "https://chrome.google.com/webstore/detail/casper-signer/djhndpllfiibmcdbnmaaahkhchcoijce";
   return (
     <div>
-      <header>Welcome to Casper Chess
-      </header>
+      <header>Welcome to Casper Chess</header>
       <h3>
         The only chess game that lets you save your win to the blockchain, so
         you can brag forever
       </h3>
-<div className="largeText">HOW IT WORKS:</div>
-<div><ol>
-    <li>Connect your Casper wallet</li>
-    <li>Match with other players in the "Game" tab for a round of chess</li>
-    <li>Whichever player loses has to pay gas to commit the game log to the blockchain</li>
-    <li>The winner gets a permanent, immutable record of their victory</li>
-    
-    </ol></div>
+      <div className="contentContainer">
+        <div className="howItWorks">
+          <div className="largeText">HOW IT WORKS:</div>
+          <div>
+            <ol>
+              <li>Connect your Casper wallet</li>
+              <li>
+                Match with other players in the "Game" tab for a round of chess
+              </li>
+              <li>
+                Whichever player loses has to pay gas to commit the game log to
+                the blockchain
+              </li>
+              <li>
+                The winner gets a permanent, immutable record of their victory
+              </li>
+            </ol>
+          </div>
+        </div>
+        <div className="noCasper">
+          <div className="largeText">Dont have a Casper account?</div>
+          <div>
+            Download the Casper Signer extension{" "}
+            <a href={wallet} target="_blank" alt="">
+              here
+            </a>
+          </div>
+          <div>set a new password to create a Vault</div>
+          <div>Then you can choose the "Create Account" option</div>
+          <div>set your name and then choose "ED25519" as the Algorithm</div>
+          <div>
+            dont forget to download your secret key so you can recover your
+            account if you forget your password
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
