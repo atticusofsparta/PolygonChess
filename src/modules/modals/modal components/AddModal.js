0d3908a164104ad89163799362423cf46d9213c0
Bobinstein <62410139+Bobinstein@users.noreply.github.com>
2022-04-12 18:01:30 -0400

    Modals roots added

---
+++ src/modules/modals/modal components/AddModal.js
@@ -0,0 +1,5 @@
+import ModalService from "./ModalService";
+
+export default function AddModal(modal){
+    ModalService.open(modal)
+}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,12 @@
-
+import AddModal from "../modules/modals/modal components/AddModal";
 import React from "react"
+import TestModal from "../modules/modals/modal files/TestModal";
 
 
 const Home = () => {return(
-    <div>About</div>
+    <div>
+        <button onClick={() => AddModal(TestModal)}>modal</button>
+    </div>
 )}
 
 export default Home;
