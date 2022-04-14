38026d8f23844a4421a961370ba6f0cb7dcb1c2e
atticusofsparta <atticusofsparta@protonmail.com>
2022-04-13 21:12:16 -0500

    trying to fix

--- server.js
+++ server.js
@@ -1,49 +1,95 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
 const bodyParser = require("body-parser");
-app.use(cors());
+const nodeAddress = "http://95.216.67.162:7777/rpc";
 
+//middleware
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-const { CasperServiceByJsonRPC,DeployUtil,EventStream,EventName,CLValueParsers,CLMap,CLValueBuilder,CLPublicKey, Contracts } = require("casper-js-sdk");
-const client = new CasperServiceByJsonRPC("http://95.216.67.162:7777/rpc");
+const contracthash = "hash-6f978f1bd5d7071d464fa3c4fe72f5c4d7aedbad6b558402ccf5d7aecbfd915b";
+const { CasperServiceByJsonRPC,DeployUtil,CLPublicKey,Contracts,CasperClient} = require("casper-js-sdk");
+const client = new CasperServiceByJsonRPC(nodeAddress);
+const deployclient = new CasperClient(nodeAddress);
 const contract = new Contracts.Contract(client);
-contract.setContractHash("hash-e951e1ca48071ebda74eed2295a2dc34ba5241bb533be9062fc9de84eda45c12");
+contract.setContractHash(contracthash);
 
+//send deploy to smart contract
+app.post('/sendDeploy', (req, res) => {
+    const signedJSON = req.body; //Get JSON from POST body
+    let signedDeploy = DeployUtil.deployFromJson(signedJSON).unwrap(); //Unwrap from JSON to Deploy object
+    signedDeploy.send(nodeAddress).then((response) => { //Send Signed Deploy
+      res.send(response); //Send this back to the frontend
+    }).catch((error) => {
+      console.log(error);
+      return;
+    });
+  });
+  //checks deploys status
+  app.get("/getDeploy", (req, res) => {
+    const hash = req.query.hash;
+    deployclient.getDeploy(hash).then((response) => { //Calls getDeploy on the client with the deploy hash. Responds with current deploy status
+      res.send(response[1].execution_results); //Send this back to the frontend
+      return;
+    }).catch((error) => {
+      res.send(error);
+      return;
+    })
+  });
 
-
+   //account balance from the last block
 app.post("/balance", async (req, res) => {
-  
   let { publicKey } = req.body;
   console.log(publicKey, "is Connected")
-
   const latestBlock = await client.getLatestBlockInfo();
   const root = await client.getStateRootHash(latestBlock.block.hash);
-
-  const balanceUref = await client.getAccountBalanceUrefByPublicKey(
-      root, 
-      CLPublicKey.fromHex(publicKey)
-      )
-
-  //account balance from the last block
-  const balance = await client.getAccountBalance(
-      latestBlock.block.header.state_root_hash,
-      balanceUref
-  );
-
+  const balanceUref = await client.getAccountBalanceUrefByPublicKey(root,CLPublicKey.fromHex(publicKey))
+  const balance = await client.getAccountBalance(latestBlock.block.header.state_root_hash,balanceUref);
     res.status(200).send(balance.toString());
 });
 
-app.post("/getAccountinfo", async (req, res) => {
-    let { publicKey } = req.body;
+//gets connected account info from contract
+app.get("/get_total_games", async (req, res) => {
+    const publicKey = req.query.publicKey;
+    console.log(CLPublicKey.fromHex(publicKey).toAccountHashStr());
     const latestBlock = await client.getLatestBlockInfo();
     const root = await client.getStateRootHash(latestBlock.block.hash);
-    const total_games = await client.getDictionaryItemByName(root, 'hash-e951e1ca48071ebda74eed2295a2dc34ba5241bb533be9062fc9de84eda45c12', '&get_caller.to_string()', 'total_games')
-    console.log(total_games);
-    res.send(); //Send this back to the frontend
+    await client.getDictionaryItemByName(root,contracthash, CLPublicKey.fromHex(publicKey).toAccountHashStr().substring(13), "total_games").then((response) => {
+    res.send(response.CLValue.data)}
+     )
+})
 
-});
+app.get("/get_wins", async (req, res) => {
+    const publicKey = req.query.publicKey;
+    console.log(CLPublicKey.fromHex(publicKey).toAccountHashStr());
+    const latestBlock = await client.getLatestBlockInfo();
+    const root = await client.getStateRootHash(latestBlock.block.hash);
+    await client.getDictionaryItemByName(root,contracthash, CLPublicKey.fromHex(publicKey).toAccountHashStr().substring(13), "wins").then((response) => {
+    res.send(response.CLValue.data)}
+     )
+})
+app.get("/get_losses", async (req, res) => {
+    const publicKey = req.query.publicKey;
+    console.log(CLPublicKey.fromHex(publicKey).toAccountHashStr());
+    const latestBlock = await client.getLatestBlockInfo();
+    const root = await client.getStateRootHash(latestBlock.block.hash);
+    await client.getDictionaryItemByName(root,contracthash, CLPublicKey.fromHex(publicKey).toAccountHashStr().substring(13), "losses").then((response) => {
+    res.send(response.CLValue.data)}
+     )
+})
+app.get("/get_stalemates", async (req, res) => {
+    const publicKey = req.query.publicKey;
+    console.log(CLPublicKey.fromHex(publicKey).toAccountHashStr());
+    const latestBlock = await client.getLatestBlockInfo();
+    const root = await client.getStateRootHash(latestBlock.block.hash);
+    await client.getDictionaryItemByName(root,contracthash, CLPublicKey.fromHex(publicKey).toAccountHashStr().substring(13), "stalemates").then((response) => {
+    res.send(response.CLValue.data)}
+     )
+})
+
+
+
 app.listen(6100, () => console.log("running on port 6100..."));
+
--- src/App.js
+++ src/App.js
@@ -16,40 +16,69 @@ import { Routes, Route, Link, Outlet } from "react-router-dom";
 import "./style.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Settings from "./pages/Settings";
 import Nopage from "./pages/Nopage";
 import Game from "./pages/Game";
 import Layout from "./pages/Layout";
 import axios from "axios";
-import ModalRoot from "./modules/modals/modal components/ModalRoot";
+//import ModalRoot from "./modules/modals/modal components/ModalRoot";
 import ModalService from "./modules/modals/modal components/ModalService";
+var getDeployInterval = null;
+
 
 const apiUrl = "http://localhost:6100/api";
 const casperService = new CasperServiceByJsonRPC(apiUrl);
 const casperClient = new CasperClient(apiUrl);
 const client = new CasperClient(apiUrl);
 const contract = new Contracts.Contract(client);
+const nftcontracthash = "hash-ec5deac7aa9f869c22d5628f1082a545d98daa6ef6289f414d655d77f4ff3e77";
 const singleplayer_contract =
   "hash-e951e1ca48071ebda74eed2295a2dc34ba5241bb533be9062fc9de84eda45c12";
 
 function App() {
-  const [balance, setBalance] = useState("");
-  const [address, setAddress] = useState("");
-  const [total_games, setTotal_Games] = useState("");
-  const [wins, setWins] = useState("");
-  const [losses, setLosses] = useState("");
-  const [stalemates, setStalemates] = useState("");
-  const [history, setHistory] = useState("");
-  const [signerConnected, setSignerConnected] = useState(false);
-  const [signerLocked, setSignerLocked] = useState(true);
-  const [activeKey, setActiveKey] = useState("");
+  const [score, setScore] = useState("");
+  const gametoapp = (gameResult) => {
+    if (gameResult === "win"){setScore("win")}
+    if (gameResult === "loss"){setScore("loss")}
+    if (gameResult === "stalemate"){setScore("stalemate")}
+    console.log("gameResult", gameResult)
+
+  }
+  
+   //connected account details
+   const [balance, setBalance] = useState("?");
+   const [address, setAddress] = useState("?");
+
+  //account game detals
+  const [nftBalance, setNftBalance] = useState(0);
+  const [total_games, setTotal_Games] = useState(0);
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+  const [stalemates, setStalemates] = useState(0);
+
+  //session game details
+  const [sessionGames, setSessionGames] = useState(0)
+  const [sessionWins, setSessionWins] = useState(0)
+  const [sessionLosses, setSessionLosses] = useState(0)
+  const [sessionStalemates, setSessionStalemates] = useState(0)  
 
 
-const globalPromiseRejectionHandler = (event) => {
-  console.log("Unhandled promise rejection reason: ", event.reason);
-}
+  useEffect(()=>{
+    
+    if (score === "win"){setSessionWins(sessionWins+1)}
+    if (score === "loss"){setSessionLosses(sessionLosses+1)}
+    if (score === "stalemate"){setSessionStalemates(sessionStalemates+1)}
+    if (score === "win" | "loss" | "stalemate"){setSessionGames(sessionGames+1)}
+    
+  },[score])
 
-window.onunhandledrejection = globalPromiseRejectionHandler;
+  
+
+
+//const globalPromiseRejectionHandler = (event) => {
+  //console.log("Unhandled promise rejection reason: ", event.reason);
+//}
+
+//window.onunhandledrejection = globalPromiseRejectionHandler;
 
 
   //   window.onunhandledrejection = function(e) {
@@ -64,49 +93,64 @@ window.onunhandledrejection = globalPromiseRejectionHandler;
       
   // });
 
-  useEffect(() => {
-    //set html
-    const textAddress = document.getElementById("textAddress");
-    const textBalance = document.getElementById("textBalance");
-    const textTotal_Games = document.getElementById("textTotal_Games");
-    textAddress.textContent = `Connected account: ${address.replace(
-      address.slice(5, 61),
-      " . . . "
-    )}`;
-    textBalance.textContent = `Balance: ${balance / 1000000000}`;
-    textTotal_Games.textContent = `Total Games: ${total_games}`;
-  }, [address, balance]);
+ //set html values
+ useEffect(()=>{ 
+  
+  const textAddress = document.getElementById('textAddress');
+  const textBalance = document.getElementById('textBalance');
+  const textTotal_Games = document.getElementById('textTotal_Games');
+  const textWins = document.getElementById('textWins');
+  const textLosses = document.getElementById('textLosses');
+  const textStalemates = document.getElementById('textStalemates');
+  textAddress.textContent = `Connected account: ${address.replace(address.slice(5, 61), ' . . . ')}`;
+  textBalance.textContent = `CSPR Balance: ${balance / 1000000000}`;
+  textTotal_Games.textContent = `Total Games: ${total_games}` + ` + ${sessionGames}`;
+  textWins.textContent = `Wins: ${wins}` + ` + ${sessionWins}`;
+  textLosses.textContent = `Losses: ${losses}` + ` + ${sessionLosses}`;
+  textStalemates.textContent = `Stalemates: ${stalemates}` + ` + ${sessionStalemates}`;
+},[address, balance, total_games, wins, losses, stalemates, sessionGames, sessionWins, sessionLosses, sessionStalemates])
 
-  useEffect(() => {
-    async function AccountInfo() {
-      const isConnected = await window.casperlabsHelper.isConnected();
-      const publicKey = await window.casperlabsHelper.getActivePublicKey();
-      
-      setAddress(publicKey);
-        ModalService.userKey = publicKey
-         console.log(ModalService.userKey)
-      
 
-      
+ //get  account data from server
+ useEffect(()=>{
+  async function AccountInfo(){
+    const publicKey = await window.casperlabsHelper.getActivePublicKey();
+          setAddress(publicKey);
+         axios.post('http://localhost:6100/balance', {"publicKey": publicKey}).then((response) => {
+           const bal = response.data;
+           setBalance(bal)
+           console.log(bal)
+         }
+         )
+         axios.get('http://localhost:6100/get_total_games', {params: {
+          publicKey: publicKey}}).then((response) => {
+            setTotal_Games(parseInt(response.data.hex, 16))   
+         }
+         )
+         axios.get('http://localhost:6100/get_wins', {params: {
+          publicKey: publicKey}}).then((response) => {
+            setWins(parseInt(response.data.hex, 16));            
+         }
+         )
+         axios.get('http://localhost:6100/get_losses', {params: {
+          publicKey: publicKey}}).then((response) => {
+            setLosses(parseInt(response.data.hex, 16));            
+         }
+         )
+         axios.get('http://localhost:6100/get_stalemates', {params: {
+          publicKey: publicKey}}).then((response) => {
+            setStalemates(parseInt(response.data.hex, 16));            
+         }
+          )
 
-      axios
-        .post("http://localhost:6100/balance", { publicKey: publicKey })
-        .then((response) => {
-          const bal = response.data;
-          setBalance(bal);
-          console.log(bal);
-        }).catch(error => {console.log(error)});
-      axios
-        .post("http://localhost:6100/getAccountinfo", { publicKey: publicKey })
-        .then((response) => {
-          const games = response.data;
-          setTotal_Games(games);
-          console.log(games);
-        }).catch(error => {console.log(error)});
-    }
-    var accountinfoint = setInterval(AccountInfo, 5000);
-    return accountinfoint;
-  }, []);
+
+        }
+  var accountinfoint = setInterval(AccountInfo, 10000)
+  return accountinfoint;
+},[])
+
+
+
 
   async function connect() {
     try {
@@ -123,36 +167,99 @@ window.onunhandledrejection = globalPromiseRejectionHandler;
     }
   }
 
-  async function deploy() {
-    if (activeKey == null) {
-      //Need to be connected to the Signer to continue
-      alert("Please unlock the signer to continue");
-      return;
-    }
+ //save button
+ async function deploy() {
 
-    const args = RuntimeArgs.fromMap({
-      //Need to build a UInt512 CLValue and package into RuntimeArgs
-      total_games: CLValueBuilder.u512(total_games),
-      wins: CLValueBuilder.u512(total_games),
-      losses: CLValueBuilder.u512(total_games),
-      stalemates: CLValueBuilder.u512(total_games),
-      history: CLValueBuilder.u512(history),
-    });
+  let totalGamesArg = total_games + sessionGames;
+  let winsArg = wins + sessionWins;
+  let lossesArg = losses + sessionLosses;
+  let stalematesArg = stalemates + sessionStalemates;
+  const args = RuntimeArgs.fromMap({ //Need to build a UInt512 CLValue and package into RuntimeArgs
+    "new_total_games":CLValueBuilder.u512(totalGamesArg),
+    "new_wins":CLValueBuilder.u512(winsArg),
+    "new_losses":CLValueBuilder.u512(lossesArg),
+    "new_stalemates":CLValueBuilder.u512(stalematesArg)       
+   }); 
 
-    // const pubkey = CLPublicKey.fromHex(activeKey); //Build CLPublicKey from hex representation of public key
-    // contract.setContractHash(singleplayer_contract); //Sets the contract hash of the Contract instance. The hash of our highscore contract
-    //const result = contract.callEntrypoint("add_game", args, pubkey, "casper-test", csprToMotes(1).toString(), [], 10000000); //Builds a Deploy object at add_highscore entrypoint
-    //const deployJSON = DeployUtil.deployToJson(result);
-    // Signer.sign(deployJSON, activeKey).then((success) => { //Initiates sign request
-    //  sendDeploy(success);
-    // }).catch((error) => {
-    //   console.log(error);
-    // });
+  const pubkey = CLPublicKey.fromHex(address); //Build CLPublicKey from hex representation of public key
+   contract.setContractHash(singleplayer_contract); //Sets the contract hash of the Contract instance. The hash of our highscore contract
+   const result = contract.callEntrypoint("add_game", args, pubkey, "casper-test", csprToMotes(1).toString(), [], 10000000); //Builds a Deploy object at add_highscore entrypoint
+   const deployJSON = DeployUtil.deployToJson(result);
+   Signer.sign(deployJSON, address).then((success) => { //Initiates sign request
+   sendDeploy(success);
+ }).catch((error) => {
+   console.log(error);
+   console.log(args)
+ });
+}
+async function sendDeploy(signedDeployJSON) {
+axios.post("http://localhost:6100/sendDeploy", signedDeployJSON, { //Sends request to /sendDeploy endpoint in server.js. Need to send deployment from the backend do to CORS policy.
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).then((response) => {
+  const hash = response.data;
+  //updateStatus("Deployed. <a target='_blank' href='https://testnet.cspr.live/deploy/" + hash + "'>View on cspr.live</a>");
+  initiateGetDeployProcedure(hash);
+}).catch((error) => {
+  alert(error);
+});
+}
+async function initiateGetDeployProcedure(hash) {
+getDeploy(hash);
+getDeployInterval = setInterval(() => { //We call this every 5 seconds to check on the status of the deploy
+  getDeploy(hash);
+}, 5000);
+}
+async function getDeploy(deployHash) {
+axios.get("http://localhost:6100/getDeploy", { //Sends request to /getDeploy endpoint in server.js.
+  params: {
+    hash: deployHash,
+  }
+}, {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}).then((response) => {
+  //response.data[0] == execution_results {...}
+  if (response.data.length == 0) { //See if there's return data yet
+    console.log("No return data yet");
+    return;
   }
 
+  const executionResults = response.data[0];
+
+  if (!executionResults.hasOwnProperty("result")) { //If executionResults doesn't contain the result key the deployment hasn't been executed by the node
+    console.log("Doesnt have result yet");
+    return;
+  }
+
+  const result = executionResults.result; //Get the result
+  console.log(response.data);
+
+  if (result.hasOwnProperty("Success")) { //Deployment succeeded!
+    console.log("Success!");
+     setSessionGames(0);
+     setSessionWins(0);
+     setSessionLosses(0);
+     setSessionStalemates(0);
+    console.log("Execution Successful")
+  } else if (result.hasOwnProperty("Failure")) {
+    console.log("Execution Failure");
+  } else {
+    console.log("Unknown Error");
+  }
+  clearInterval(getDeployInterval); //Stop polling getDeploy
+
+}).catch((error) => {
+  alert(error);
+  clearInterval(getDeployInterval); //Stop polling getDeploy
+});
+}
+// <ModalRoot/>
   return (
     <div>
-      <ModalRoot/>
+     
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/Home">
@@ -163,43 +270,43 @@ window.onunhandledrejection = globalPromiseRejectionHandler;
             <Layout />
             <li className="nav-item">
               <a className="nav-link" id="connectBtn" onClick={() => connect()}>
-                Connect Signer
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link"
-                id="disconnectBtn"
-                onClick={() => disconnect()}
-              >
-                Disconnect Signer
+                Connect
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" id="deployBtn" onClick={() => deploy()}>
-                Save to Chain
+                Save
               </a>
             </li>
           </ul>
 
-          <ul>
-            <li id="textAddress">Connected account: </li>
-            <li id="textBalance">Balance: </li>
-            <li id="textTotal_Games">Total Games: </li>
-          </ul>
+          <ul >  
+         <li id="textAddress">Connected account: </li>
+                  <li id="textBalance">CSPR Balance: </li>
+                </ul>
+                <ul>
+                <li id="textTotal_Games">Total Games: </li>
+                <li id="textWins">Wins: </li>
+                </ul>
+                <ul>
+                <li id="textLosses">Losses: </li>
+                <li id="textStalemates">Stalemates: </li>
+                </ul>
         </div>
       </nav>
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route index element={<Home />} />
         <Route path="/About" element={<About />} />
-        <Route path="/Game" element={<Game />} />
-        <Route path="/Settings" element={<Settings />} />
+        <Route path="/Game" element={<Game gametoapp={gametoapp}/>} />
         <Route path="*" element={<Nopage />} />
       </Routes>
       <Outlet />
     </div>
   );
+}
+function csprToMotes(cspr) {
+  return cspr * 10**9;
 }
 
 export default App;
